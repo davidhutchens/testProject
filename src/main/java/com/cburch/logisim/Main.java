5a1eedf8e257683566742ce8d386775989a01c71
David Hutchens <David.Hutchens@millersville.edu>
2023-03-01 14:37:26 -0500

    Greeting expressive

--- src/main/java/com/cburch/logisim/Main.java
+++ src/main/java/com/cburch/logisim/Main.java
@@ -2,6 +2,6 @@ package com.cburch.logisim;
 
 class Main {
    public static void main (String[] args) {
-      System.out.println("Hello world");
+      System.out.println("Hello world!");
    }
 }
