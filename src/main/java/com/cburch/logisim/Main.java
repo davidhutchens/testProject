a98578129ed8676417327d1fc6d352cf0258304a
David Hutchens <David.Hutchens@millersville.edu>
2026-08-03 14:19:09 -0400

    Minor change to Main.java.

--- src/main/java/com/cburch/logisim/Main.java
+++ src/main/java/com/cburch/logisim/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 class Main {
   public static void main(String[] args) {
     try (FileOutputStream fos = new FileOutputStream("/Users/hutchens/testOutput.txt")) {
-      String text = "It seems to work!\n";
+      String text = "It seems to still work!\n";
       byte[] myBytes = text.getBytes();
       fos.write(myBytes);
       fos.close();
